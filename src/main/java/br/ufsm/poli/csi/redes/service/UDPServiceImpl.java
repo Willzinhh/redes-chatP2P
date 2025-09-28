28e36b937c00b97c8337c5e83fe020875c334b24
nicolegg13 <nicolegrazzioli@gmail.com>
2025-09-28 15:48:05 -0300

    1o commit

---
+++ src/main/java/br/ufsm/poli/csi/redes/service/UDPServiceImpl.java
@@ -0,0 +1,25 @@
+package br.ufsm.poli.csi.redes.service;
+
+import br.ufsm.poli.csi.redes.model.Usuario;
+
+public class UDPServiceImpl implements UDPService {
+    @Override
+    public void enviarMensagem(String mensagem, Usuario destinatario, boolean chatGeral) {
+
+    }
+
+    @Override
+    public void usuarioAlterado(Usuario usuario) {
+
+    }
+
+    @Override
+    public void addListenerMensagem(UDPServiceMensagemListener listener) {
+
+    }
+
+    @Override
+    public void addListenerUsuario(UDPServiceUsuarioListener listener) {
+
+    }
+}
